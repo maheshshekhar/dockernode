099e66fcdd45729f3ef55b428fc7206d88d75f1c
System Administrator <root@Mahesh-Shekhars-MacBook-Pro.local>
2016-01-19 18:29:50 +0530

    all

---
+++ views/jenkins.js
@@ -0,0 +1,3 @@
+aws bucket updated with the tar file
+After the changes made this is the latest file
+after the sudo removed
---
+++ views/test.js
@@ -0,0 +1,4 @@
+All Success 
+build to junkins
+uploaded to aws s3
+ALl done
